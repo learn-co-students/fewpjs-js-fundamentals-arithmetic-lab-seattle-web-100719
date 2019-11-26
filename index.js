902034adf758319ecc19eecbf810a561383cbb85
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-26 13:41:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+function multiple(num1, num2)
+{
+    return num1 * num2;
+}
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.random();
+
+let mod = 10%6;
+
+let max = Math.max(1, 20);
